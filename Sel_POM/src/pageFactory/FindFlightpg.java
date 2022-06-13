1d840e30e4223e59a663776a87ba1ef4c50de314
akhilrathore17 <akhilrathore17@gmail.com>
2022-06-13 19:05:54 +0530

    a

--- Sel_POM/src/pageFactory/FindFlightpg.java
+++ Sel_POM/src/pageFactory/FindFlightpg.java
@@ -1,5 +1,5 @@
-package pageFactory;
-
+package pagefactory;
+// test comment to VSCode to github
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.CacheLookup;
--- Sel_POM/src/testCase/Runner.java
+++ Sel_POM/src/testCase/Runner.java
@@ -1,11 +1,8 @@
-package testCase;
+package testcase;
 
 import org.testng.annotations.Test;
-
 import pageFactory.*;
-
 import java.util.concurrent.TimeUnit;
-
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.*;
